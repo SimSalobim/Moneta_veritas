7068718f9c2ecf9519de1bb3c7e3bbb3290c79e9
Roman Fedotov <paionn@mail.ru>
2025-11-08 00:14:16 +0300

    change views and templates

--- moneta_veritas/homepage/views.py
+++ moneta_veritas/homepage/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from catalog.models import Coin, Banknote
 
 # Create your views here.
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    coin_list = Coin.objects.values(
+        'id', 'name', 'category', 'denomination', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+    )
+    banknote_list = Banknote.objects.values(
+        'id', 'name', 'category', 'denomination', 'description'
+    ).filter(
+        is_published=True,
+        is_on_main=True,
+    )
+    
+    context = {
+        'coin_list': coin_list,
+        'banknote_list': banknote_list
+    }
+    return render(request, template, context)
